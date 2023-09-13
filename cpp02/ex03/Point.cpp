29d70a59ff15bf3c09e239678614af0203e705e2
rsoo23 <rongjie.soo12@gmail.com>
2023-09-13 09:31:08 +0800

    cpp02 - ex03 completed

--- cpp02/ex03/Point.cpp
+++ cpp02/ex03/Point.cpp
@@ -6,34 +6,31 @@
 /*   By: rsoo <rsoo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 10:16:13 by rsoo              #+#    #+#             */
-/*   Updated: 2023/09/12 23:32:36 by rsoo             ###   ########.fr       */
+/*   Updated: 2023/09/13 09:18:36 by rsoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.h"
 
 // default constructor
-Point::Point(void): x(0), y(0) {}
+Point::Point(void): x(Fixed(0)), y(Fixed(0)) {}
 
 // float constructor
-Point::Point(const float f1, const float f2): x(f1), y(f2) {}
+Point::Point(const float f1, const float f2): x(Fixed(f1)), y(Fixed(f2)) {}
 
 // copy constructor
-Point::Point(const Point& other) {
-	*this = other;
-}
+Point::Point(const Point& other): x(other.getXCoord()), y(other.getYCoord()) {}
 
 // copy assignment constructor
-// - if you want to remove the const, cast the var to the data type w/o const
+// - if you want to remove the const, cast the var to the data type w/o const (error)
+// - you cannot change const variables, so just return *this in all cases
 Point& Point::operator=(const Point& other) {
-	if (this != &other) {
-		(Fixed)this->x = other.getXCoord();
-		(Fixed)this->y = other.getYCoord();
-	}
-	return *this;	
+	if (this == &other)
+		return *this;
+	return *this;
 }
 
-// destructoru
+// destructor
 Point::~Point(void) {}
 
 // getters
@@ -47,6 +44,6 @@ const Fixed Point::getYCoord(void) const {
 
 // << operator overload
 std::ostream& operator<<(std::ostream& out, const Point& pt) {
-	out << "x: hi " << pt.getXCoord() << ", y: " << pt.getYCoord();
+	out << "x: " << pt.getXCoord() << ", y: " << pt.getYCoord();
 	return (out);
 }
--- cpp02/ex03/main.cpp
+++ cpp02/ex03/main.cpp
@@ -6,20 +6,20 @@
 /*   By: rsoo <rsoo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/28 10:14:07 by rsoo              #+#    #+#             */
-/*   Updated: 2023/09/12 23:48:48 by rsoo             ###   ########.fr       */
+/*   Updated: 2023/09/13 09:30:17 by rsoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.h"
 
 int main( void ) {
-	Point a(0.0f , 0.0f);
-	Point b(10.0f , 0.0f);
-	Point c(0.0f , 10.0f);
+	Point a(10.0f , 10.0f);
+	Point b(-10.0f , 0.0f);
+	Point c(10.0f , 10.0f);
 	
-	Point point(25.0f, 25.0f);
-	
-	std::cout << YELLOW << "TESTS: "<< std::endl;
+	Point point(3, 3);
+
+	std::cout << YELLOW << "Points: "<< std::endl;
 	std::cout << CYAN << "Vertex 1: " << RESET << a << std::endl;
 	std::cout << GREEN << "Vertex 2: " << RESET << b << std::endl;
 	std::cout << BLUE << "Vertex 3: " << RESET << c << std::endl;
